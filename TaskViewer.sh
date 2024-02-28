6527011d4a6ea3c59ea463dd0f9b2b3ba317ec34
Emil <30472052+hasanzadee@users.noreply.github.com>
2024-02-29 02:21:15 +0400

    TaskViewer.sh

--- TaskViewer.sh
+++ TaskViewer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+yum install dialog -y
 scripts=("task1.sh" "task2.sh" "task3.sh" "task4.sh" "task5.sh" "task6.sh" "task7.sh" "task8.sh" "task9.sh" "task10.sh" "task11.sh" "task12.sh" "task13.sh" "task14.sh" "task15.sh")
 CMD=(dialog --title "Scripts menu" --menu "Choose the script:" 22 76 16)
 
